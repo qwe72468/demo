b19b34c06c8f201044303145f4ed4ae3a1dd504c
17610170283 <ljt405192@163.com>
2023-03-25 12:57:01 +0800

    jar

--- api/src/main/java/com/example/api/controller/DemoController.java
+++ api/src/main/java/com/example/api/controller/DemoController.java
@@ -4,13 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @Slf4j
 @RestController
 public class DemoController {
 
     @GetMapping(value = "/index")
-   public Integer demoMethod(){
-       return 324;
-   }
+    public Integer demoMethod() {
+
+        log.info("DemoController demoMethod, " + new Date().getTime());
+        return 324;
+    }
 
 }
